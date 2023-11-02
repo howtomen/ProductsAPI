07a94d769170feec7348c6de1f151a8b34d30f89
Daniel Ariza <danielariza@csu.fullerton.edu>
2023-11-02 11:50:12 -0400

    initial skeleton for API

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+)
+
+var (
+	DB_username string
+	DB_pass     string
+	DB_name     string
+)
+
+func main() {
+	//Get Database info from flags.
+	flag.StringVar(&DB_username, "username", "postgres", "Username for PSQL DB")
+	flag.StringVar(&DB_pass, "password", "password123", "Password for PSQL DB")
+	flag.StringVar(&DB_name, "db", "Products-1", "Name of the PSQL DB")
+	flag.Parse()
+
+	a := App{}
+	a.Initialize(
+		DB_username,
+		DB_pass,
+		DB_name)
+
+	a.Run(":8010")
+}
